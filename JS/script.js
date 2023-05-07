096e4460f348f5f1fd2a6bca31521b228a63259d
hkhuy <21135421.huy@student.iuh.edu.vn>
2023-05-07 19:21:45 +0700

    update

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,35 @@ const comment = document.querySelector('#list-comment')
 const commentItem = document.querySelectorAll('#list-comment .item')
 var translateY = 0
 var count = commentItem.length
+const inputElement = document.getElementById('search');
+function search(inputValue){
+  if (inputValue == "vải" || inputValue == "vải nhập khẩu"){
+    alert("Vải nhập khẩu có xuất xứ tại Mỹ và có giá 59.000VNĐ")
+  } else if (inputValue == "táo" || inputValue == "táo nhập khẩu"){
+    alert("Táo nhập khẩu có xuất xứ tại Mỹ và có giá 69.000VNĐ")
+  } else if (inputValue == "cà chua" || inputValue == "cà chua đà lạt"){
+    alert("Cà chua Đà Lạt có xuất xứ tại Việt Nam và có giá 79.000VNĐ")
+  } else if (inputValue == "bom" || inputValue == "bom mỹ"){
+    alert("Bom Mỹ có xuất xứ tại Mỹ và có giá 179.000VNĐ")
+  } else if (inputValue == "dâu" || inputValue == "dâu đà lạt"){
+    alert("Dâu Đà Lạt có xuất xứ tại Việt Nam và có giá 249.000VNĐ")
+  } else if (inputValue == "ổi" || inputValue == "ổi đài loan"){
+    alert("Ổi Đài Loan có xuất xứ tại Đài Loan và có giá 59.000VNĐ")
+  } else{
+    alert("Không tìm thấy sản phẩm")
+  }
+  
+}
+// Lấy tham chiếu đến phần tử button
+const buttonElement = document.querySelector('button[type="submit"]');
+
+// Đăng ký sự kiện click cho button và gắn kết với hàm search()
+buttonElement.addEventListener('click', function() {
+  const inputValue = document.getElementById('search').value;
+  search(inputValue);
+});
+
+
 
 next.addEventListener('click', function (event) {
   event.preventDefault()
